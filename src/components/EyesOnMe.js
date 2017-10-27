705a4be14187cd801bd39b06fbb4ed70c9f649a0
Han Lee <hanlee12@gmail.com>
2017-10-27 14:46:52 -0400

    half done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  inputPassword = () => {
+
+  }
+
+  render() {
+    return (
+      <input onClick={this.inputPassword} type='password'>
+      </input>
+    )
+
+  }
+}
+
+export default EyesOnMe;
